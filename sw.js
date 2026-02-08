3edde65e94ff4b3c0c90524b4de773f8fbc216f3
Eliab Campos <campeao.mkt.user@gmail.com>
2026-02-08 12:25:02 -0400

    Alterção no PWA

--- sw.js
+++ sw.js
@@ -1,23 +1,40 @@
+const CACHE_NAME = 'keymaster-v2';
 
-const CACHE_NAME = 'keymaster-v1';
 const ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
 ];
 
+// INSTALL
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
+// ACTIVATE
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH (SPA SAFE)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() =>
+      caches.match('/index.html')
+    )
   );
 });
